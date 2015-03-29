aa699563cb8dbe8b0bdd1177f4c45cd7adcc8e02
Miguel Parramon <miguel@ztory.com>
2015-03-30 01:31:40 +0200

    Switch db to sqlite + mysql

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150329133957) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "channels", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
